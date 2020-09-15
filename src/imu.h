ad395d2fa4657b7089ac5d789f28d55e3171f773
Liu Kun <liukun0104@gmail.com>
2020-09-15 11:11:02 +0800

    生成带噪声和偏置的 IMU 数据

--- src/imu.h
+++ src/imu.h
@@ -12,43 +12,45 @@
 
 #include "param.h"
 
-struct MotionData
-{
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    double timestamp;
-    Eigen::Matrix3d Rwb;
-    Eigen::Vector3d twb;
-    Eigen::Vector3d imu_acc;
-    Eigen::Vector3d imu_gyro;
+struct MotionData {
+  // eigen 对齐 new 方法宏定义
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  double timestamp;
+  // 世界系下机体系的 R t
+  Eigen::Matrix3d Rwb;
+  Eigen::Vector3d twb;
 
-    Eigen::Vector3d imu_gyro_bias;
-    Eigen::Vector3d imu_acc_bias;
+  // 陀螺仪 和 加速度计 读数(带有偏置和白噪声)
+  Eigen::Vector3d imu_acc;
+  Eigen::Vector3d imu_gyro;
 
-    Eigen::Vector3d imu_velocity;
+  //陀螺仪 和 加速度计 偏置(带有误差)
+  Eigen::Vector3d imu_gyro_bias;
+  Eigen::Vector3d imu_acc_bias;
+
+  // 世界系下机体系的速度
+  Eigen::Vector3d imu_velocity;
 };
 
 // euler2Rotation:   body frame to interitail frame
-Eigen::Matrix3d euler2Rotation( Eigen::Vector3d  eulerAngles);
+Eigen::Matrix3d euler2Rotation(Eigen::Vector3d eulerAngles);
 Eigen::Matrix3d eulerRates2bodyRates(Eigen::Vector3d eulerAngles);
 
+class IMU {
+ public:
+  IMU(Param p);
+  Param param_;
+  Eigen::Vector3d gyro_bias_;
+  Eigen::Vector3d acc_bias_;
 
-class IMU
-{
-public:
-    IMU(Param p);
-    Param param_;
-    Eigen::Vector3d gyro_bias_;
-    Eigen::Vector3d acc_bias_;
+  Eigen::Vector3d init_velocity_;
+  Eigen::Vector3d init_twb_;
+  Eigen::Matrix3d init_Rwb_;
 
-    Eigen::Vector3d init_velocity_;
-    Eigen::Vector3d init_twb_;
-    Eigen::Matrix3d init_Rwb_;
+  MotionData MotionModel(double t);
 
-    MotionData MotionModel(double t);
-
-    void addIMUnoise(MotionData& data);
-    void testImu(std::string src, std::string dist);        // imu数据进行积分，用来看imu轨迹
-
+  void addIMUnoise(MotionData& data);
+  void testImu(std::string src, std::string dist);  // imu数据进行积分，用来看imu轨迹
 };
 
-#endif //IMUSIMWITHPOINTLINE_IMU_H
+#endif  //IMUSIMWITHPOINTLINE_IMU_H
--- src/param.h
+++ src/param.h
@@ -7,43 +7,40 @@
 
 #include <eigen3/Eigen/Core>
 
-class Param{
+class Param {
+ public:
+  Param();
 
-public:
+  // time
+  int imu_frequency   = 200;
+  int cam_frequency   = 30;
+  double imu_timestep = 1. / imu_frequency;
+  double cam_timestep = 1. / cam_frequency;
+  double t_start      = 0.;
+  double t_end        = 20;  //  20 s
 
-    Param();
+  //连续时间-噪声密度-标准差，单位 (rad/s) * (1/√hz)
+  double gyro_noise_sigma = 0.015; 
+  //　m/((s^2) * 1/sqrt(hz))
+  double acc_noise_sigma  = 0.019;  
 
-    // time
-    int imu_frequency = 200;
-    int cam_frequency = 30;
-    double imu_timestep = 1./imu_frequency;
-    double cam_timestep = 1./cam_frequency;
-    double t_start = 0.;
-    double t_end = 20;  //  20 s
+  // 连续时间-IMU 偏置变化率-标准差，
+  double gyro_bias_sigma = 1.0e-5;  //单位 (rad/s^2)*(1/√hz)
+  double acc_bias_sigma  = 0.0001;  //单位 (m/s^3)*(1/√hz)
 
-    // noise
-    double gyro_bias_sigma = 1.0e-5;
-    double acc_bias_sigma = 0.0001;
+  double pixel_noise = 1;  // 1 pixel noise
 
-    double gyro_noise_sigma = 0.015;    // rad/s * 1/sqrt(hz)
-    double acc_noise_sigma = 0.019;      //　m/(s^2) * 1/sqrt(hz)
+  // cam f
+  double fx      = 460;
+  double fy      = 460;
+  double cx      = 255;
+  double cy      = 255;
+  double image_w = 640;
+  double image_h = 640;
 
-    double pixel_noise = 1;              // 1 pixel noise
-
-    // cam f
-    double fx = 460;
-    double fy = 460;
-    double cx = 255;
-    double cy = 255;
-    double image_w = 640;
-    double image_h = 640;
-
-
-    // 外参数
-    Eigen::Matrix3d R_bc;   // cam to body
-    Eigen::Vector3d t_bc;     // cam to body
-
+  // 外参数
+  Eigen::Matrix3d R_bc;  // cam to body
+  Eigen::Vector3d t_bc;  // cam to body
 };
 
-
-#endif //IMUSIM_PARAM_H
+#endif  //IMUSIM_PARAM_H
